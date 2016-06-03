4876835eead29f4605bb0ce46e0b0301fcb297a7
Jasper Valero <contact@jaspervalero.com>
2016-06-02 18:15:52 -0700

    Add nav links to header, prep for conversion to container

--- src/components/header.js
+++ src/components/header.js
@@ -7,6 +7,7 @@
  */
 import React from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
 
@@ -16,10 +17,39 @@ class Header extends React.Component {
 				<Link to="/" className="navbar-brand">
 					<span className="todo-icon glyphicon glyphicon-th-list" /> TODO
 				</Link>
+				<ul className="nav navbar-nav">
+					{ this.renderLinks() }
+				</ul>
 			</nav>
 		);
 	}
 
+	renderLinks() {
+		if ( ! this.props.authenticated ) {
+			return [
+				<li className="nav-item" key={ 1 }>
+					<Link to="/signin" className="nav-link">Sign In</Link>
+				</li>,
+				<li className="nav-item" key={ 2 }>
+					<Link to="/signup" className="nav-link">Sign Up</Link>
+				</li>
+			];
+		}
+
+		return [
+			<li className="nav-item" key={ 1 }>
+				<Link to="/boards" className="nav-link">Boards</Link>
+			</li>,
+			<li className="nav-item" key={ 2 }>
+				<Link to="/signout" className="nav-link">Sign Out</Link>
+			</li>
+		];
+	}
+
+}
+
+function mapStateToProps( state ) {
+	return { authenticated: state.auth.authenticated };
 }
 
 export default Header;
